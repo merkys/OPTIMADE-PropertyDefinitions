59679f40f0b8a60aa3c19d16af260cb5c2131804
Andrius Merkys <andrius.merkys@gmail.com>
2024-03-22 11:28:28 +0200

    Adding constructors.

--- lib/OPTIMADE/PropertyDescriptions.pm
+++ lib/OPTIMADE/PropertyDescriptions.pm
@@ -3,4 +3,15 @@ package OPTIMADE::PropertyDescriptions;
 use strict;
 use warnings;
 
+sub new
+{
+    my( $class, $path ) = @_;
+    return bless { path => $path }, $class;
+}
+
+sub entry_types()
+{
+    my( $self ) = @_;
+}
+
 1;
--- lib/OPTIMADE/PropertyDescriptions/EntryType.pm
+++ lib/OPTIMADE/PropertyDescriptions/EntryType.pm
@@ -3,4 +3,10 @@ package OPTIMADE::PropertyDescriptions::EntryType;
 use strict;
 use warnings;
 
+sub new
+{
+    my( $parent, $name ) = @_;
+    return bless { parent => $parent, name => $name }, $class;
+}
+
 1;
